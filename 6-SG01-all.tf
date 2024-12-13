afa164323730e8f5e02bbf87c220f263c27dc45c
Jerome Hunter Sr. <Firstofmyname5802@gmail.com>
2024-12-12 19:33:21 -0600

    Create 6-SG01-all.tf

---
+++ 6-SG01-all.tf
@@ -0,0 +1,13 @@
+resource "aws_security_group" "app1-sg01-servers" {
+  name        = "app1-sg01-servers"
+  description = "app1-sg01-servers"
+  vpc_id      = aws_vpc.app1.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
